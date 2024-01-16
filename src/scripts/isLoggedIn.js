41f308652ae069d910b7f9f2129f1e7a120ba852
John Stockton <johnstocktoniv@gmail.com>
2024-01-15 21:14:10 -0500

    fix bad variable assignment

--- src/scripts/isLoggedIn.js
+++ src/scripts/isLoggedIn.js
@@ -1,6 +1,6 @@
 export default function isLoggedIn() {
     const isNull = (sessionStorageName) => { 
-        const value = sessionStorageName.getItem(sessionStorageName)
+        const value = sessionStorage.getItem(sessionStorageName)
         return (value != null && value != "undefined")
     }
     return isNull("authToken") || isNull("accessToken")
